5f2021f86ce8adf918f26daa417141d666861883
karlupo <73790544+karlupo@users.noreply.github.com>
2022-07-07 11:47:26 +0200

    Delete terra_revivestation directory

--- terra_revivestation/locales/de.lua
+++
@@ -1,5 +0,0 @@
-Locales['de'] = {
-    ['sendername'] = 'Bankberater',
-    ['sendercompany'] = 'Los Santos Zentralbank',
-    ['bought_msg'] = 'Sie haben einen Betrag von %s$ an das Medical Department überwiesen. Schönen Tag noch!',
-}
