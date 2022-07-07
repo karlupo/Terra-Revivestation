164816edb4a2795e9762be7acc38f067d1171a50
karlupo <73790544+karlupo@users.noreply.github.com>
2022-07-07 22:11:25 +0200

    Min amount of Medics added
    You can now set when the revive station shouldnt be avalaible anymore if there are too many medics online.

---
+++ terra_revivestation/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+Config.x = -463.72
+Config.y = -339.04
+Config.z = 34.5
+Config.Price = 10000
+Config.ReviveRange = 6
+Config.AccessRange = 1
+Config.MedicJobName = "ambulance"
+Config.MinMedicsToNotShow = 1
